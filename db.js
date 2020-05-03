c164bafd122bf8dc6c568214f67ca8120ea02af8
amansaxna <aman.saxena2898@gmail.com>
2020-05-03 08:03:22 +0530

    preparing heuroko

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const MongoClient = require("mongodb").MongoClient;
 const ObjectID = require("mongodb").ObjectID;
 const dbname = "car_retail";
-const url = process.env.MONGODB_URI || "mongodb://localhost:27017";
+const url = "mongodb://localhost:27017";
 const mongoOptions = {useNewUrlParser : true}
 
 //initialize the db sate to null
