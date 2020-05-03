bbfe510c4c7146d629f5517e901334f0de23de91
amansaxna <aman.saxena2898@gmail.com>
2020-05-03 09:14:44 +0530

    preparing heuroko

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const MongoClient = require("mongodb").MongoClient;
 const ObjectID = require("mongodb").ObjectID;
 const dbname = "car_retail";
-const url = "mongodb://localhost:27017";
+const url = "mongodb+srv://aman:OcKQGqq38Zlq0KW2@cluster0-cpgnw.mongodb.net/test?retryWrites=true&w=majority" || "mongodb://localhost:27017";
 const mongoOptions = {useNewUrlParser : true}
 
 //initialize the db sate to null
@@ -9,6 +9,16 @@ const state ={
     db : null
 };
 
+/*
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://aman:<password>@cluster0-cpgnw.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+*/ 
 const connect = (cb) => {
     if(state.db)//if connection exists
         cb();
